90bab86ace666b110104d21170a94a2ed150177f
vlbandara <137267283+vlbandara@users.noreply.github.com>
2024-03-16 09:56:29 +0530

    initial commit
    initial onfigurations

---
+++ local_config_server/src/main/java/com/aggrandizer/BrainiacBox/local_config/LocalConfigApplication.java
@@ -0,0 +1,13 @@
+package com.aggrandizer.BrainiacBox.local_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalConfigApplication.class, args);
+	}
+
+}
---
+++ local_config_server/src/test/java/com/aggrandizer/BrainiacBox/local_config/LocalConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggrandizer.BrainiacBox.local_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
